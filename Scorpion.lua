e317d9bbcf419235c62a0e5169eebc32e52b23dd
G-MX2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-09-28 23:36:28 -0600

    Update Scorpion.lua

--- Scorpion.lua
+++ Scorpion.lua
@@ -1,1 +1,15 @@
+local url = "https://raw.githubusercontent.com/SoyAdriYT/Scorpion/refs/heads/main/Games/Blade%20Ball.lua"
+local success, result = pcall(function()
+    return game:HttpGet(url)
+end)
 
+if success then
+    local successScript, err = pcall(function()
+        loadstring(result)()
+    end)
+    if not successScript then
+        warn("Error executing the script: "..err)
+    end
+else
+    warn("Error fetching the content from the URL.")
+end
