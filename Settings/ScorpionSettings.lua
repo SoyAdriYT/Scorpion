042f1bb4a9a37b368b18d7b1a3eb0fc1007a0358
G-MX2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-09-30 21:05:48 -0600

    Update ScorpionSettings.lua

--- Settings/ScorpionSettings.lua
+++ Settings/ScorpionSettings.lua
@@ -2,6 +2,8 @@ local _env = getgenv and getgenv() or {}
 
 local Ver = "Version: 1.1"
 local Dis = "https://discord.gg/yZTduXkhMn"
+local Git = "https://github.com/SoyAdriYT/Scorpion"
 
 _env.Discord = Dis
 _env.Version = Ver
+_env.GitHub = Git
