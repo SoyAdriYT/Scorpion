a1903d444cb9550e6bf094aec557d854d46de401
G-MX2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-10-05 20:09:49 -0600

    Update Scorpion.lua

--- Scorpion.lua
+++ Scorpion.lua
@@ -1,4 +1,4 @@
-local scriptUrl = "https://raw.githubusercontent.com/SoyAdriYT/Scorpion/refs/heads/main/Games/Murderes%20VS%20Sheriff%20Duels.lua"
+local scriptUrl = "https://raw.githubusercontent.com/SoyAdriYT/Scorpion/refs/heads/main/Games/Murder%20Mistery%202.lua"
 
 local success, errorMessage = pcall(function()
     local response = game:HttpGet(scriptUrl)
